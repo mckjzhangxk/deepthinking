2eeea47d861fed25de10970db4d9e76de6d82966
mckjzhangxk <42412462+mckjzhangxk@users.noreply.github.com>
2023-05-23 15:37:27 +0800

    imageic

--- deepthinking/growthree.py
+++ deepthinking/growthree.py
@@ -2,9 +2,9 @@
 
 # %% auto 0
 __all__ = ['torch_device', 'tree_prompts', 'hparam', 'release_cache', 'image_grid', 'isColab', 'HiperParameter', 'pil2Latents',
-           'latents2Pil', 'generate_baseon_prompt_emb']
+           'latents2Pil', 'batchPil2Latents', 'batchLatents2Pil', 'generate_baseon_prompt_emb']
 
-# %% ../nbs/growtree.ipynb 2
+# %% ../nbs/growtree.ipynb 3
 import torch,numpy as np,math,matplotlib.pyplot as plt
 import torchvision.transforms as tfms
 from diffusers import StableDiffusionPipeline,AutoencoderKL,UNet2DConditionModel,LMSDiscreteScheduler
@@ -15,13 +15,12 @@ from tqdm.auto import tqdm
 from dataclasses import dataclass
 from functools import partial
 
-# %% ../nbs/growtree.ipynb 4
+# %% ../nbs/growtree.ipynb 5
 torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def release_cache():
     gc.collect()
     torch.cuda.empty_cache()
-
 #把图片排成 rows,cols的网格中，先排cols,后排rows
 #其中len(imgs)=cols x rows
 def image_grid(imgs, rows, cols):
@@ -40,8 +39,9 @@ def isColab():
     else:
         print("当前环境不是Colab")
     return IN_COLAB
+isColab(),torch_device
 
-# %% ../nbs/growtree.ipynb 6
+# %% ../nbs/growtree.ipynb 7
 @dataclass
 class HiperParameter():
     prompts:tuple
@@ -65,7 +65,7 @@ tree_prompts =(
 )
 hparam=HiperParameter(prompts=tree_prompts)
 
-# %% ../nbs/growtree.ipynb 10
+# %% ../nbs/growtree.ipynb 11
 @torch.no_grad()
 def pil2Latents(input_image:Image)-> torch.FloatTensor:
     '''
@@ -75,7 +75,7 @@ def pil2Latents(input_image:Image)-> torch.FloatTensor:
     def tfms2Latent(r):
         return 2*r-1
     ts=tfms.ToTensor()(input_image).unsqueeze(0)
-    ts=tfms2Latent(ts).to(vae.device)
+    ts=tfms2Latent(ts).to(vae.device,dtype=vae.dtype)
     return 0.18215*vae.encode(ts).latent_dist.sample()
 @torch.no_grad()
 def latents2Pil(latents:torch.FloatTensor) ->Image:
@@ -94,7 +94,38 @@ def latents2Pil(latents:torch.FloatTensor) ->Image:
     arr_img=arr_img.astype('uint8')
     return Image.fromarray(arr_img)
 
-# %% ../nbs/growtree.ipynb 15
+@torch.no_grad()
+def batchPil2Latents(imgs:list)-> torch.FloatTensor:
+    '''
+    批量把图片转换成vae的输入，生成的tensor已经移torch_device上了
+    返回：size=[B,4,64,64]
+    '''
+    def tfms2Latent(r):
+        return 2*r-1
+    f=tfms.ToTensor()
+    
+    ts=torch.stack([f(img) for img in imgs])
+    ts=tfms2Latent(ts).to(vae.device,dtype=vae.dtype)
+    return 0.18215*vae.encode(ts).latent_dist.sample()
+@torch.no_grad()
+def batchLatents2Pil(latents:torch.FloatTensor) ->Image:
+    '''
+    批量把隐变量还原成PIL.Image
+    latents: FloatTensor,size=[B,4,64,64]
+    '''
+    def tfms2Img(r):
+        r=(0.5*r+0.5)
+        return r.float().clamp(0,1)
+    decode_imgs=vae.decode(latents/0.18215).sample.detach().cpu()
+    decode_imgs=decode_imgs.permute(0,2,3,1)
+    
+    decode_imgs=tfms2Img(decode_imgs)
+
+    arr_img=decode_imgs.numpy()*255
+    arr_img=arr_img.astype('uint8')
+    return [Image.fromarray(a) for a in arr_img]
+
+# %% ../nbs/growtree.ipynb 17
 def generate_baseon_prompt_emb(img_latent:torch.FloatTensor,token_emb:torch.FloatTensor,pbar=None)->(Image,torch.FloatTensor):
     '''
         :param token_emb: img_latent 图像的隐变量,(2,4,64,64)的tensor
@@ -103,6 +134,9 @@ def generate_baseon_prompt_emb(img_latent:torch.FloatTensor,token_emb:torch.Floa
     '''
     if hparam.seed>=0:
         torch.manual_seed(hparam.seed)
+    else:
+        torch.seed()
+
     start_step=hparam.start_step
     schedular.set_timesteps(hparam.num_inference)
     
